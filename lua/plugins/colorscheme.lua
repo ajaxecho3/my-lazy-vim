1d25d3cdb8a6b27ac896ed803f79331cfddba6f0
Bernardino Ochoa <bernardo.ochoa@concentrix.com>
2024-03-09 03:09:21 +0800

    initial setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        keywords = {
+          italic = true,
+        },
+        comments = {
+          italic = true,
+        },
+      },
+    },
+  },
+}
